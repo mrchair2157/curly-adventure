da0319c5a7663e274206b41145ec61a6ce421b8c
mrchair2157 <64299722+mrchair2157@users.noreply.github.com>
2021-06-07 23:09:32 +0000

    Add files via upload
    The first files and the beginning of a User interface.

---
+++ info.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int help() {
+    printf("h is for help and this is it.\n");
+    printf("q to quite.\n");
+    //printf("h for help.\n");
+    //printf("h for help.\n");
+    //printf("h for help.\n");
+    //printf("h for help.\n");
+    //printf("h for help.\n");
+    //printf("h for help.\n");
+    //printf("h for help.\n");
+    //printf("h for help.\n");
+}
---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "basic.h"
+#include "info.h"
+int x = 0;
+char operator;
+int main() {
+
+for(int z = 1;z == 1;) {
+printf("%d\n",x);
+scanf("%c",&operator);    
+    switch(operator) {
+        
+        case 'h':
+        help();
+        x--;
+        break;    
+        
+        case 'q':
+        return 0;
+
+    }
+x++;    
+if(x == 5) {
+    printf("enter h for help\n");
+    x = 0;
+    }
+}
+return 0;
+}
+
